e21d90009a611f97902cbc0bddd81c02d5c3a83d
Wojti <wdziuba@student.agh.edu.pl>
2019-06-12 00:08:06 +0200

    Siup

---
+++ EndlessRunner/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+    public float destroyAtX;
+
+    public GameObject deathEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= destroyAtX)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("ObjectRemover"))
+        {
+            other.GetComponent<PlayerControler>().health -= damage;
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
